a14a70d8447cef399a7f9c0d046a0bfd938818a9
RiTiK <168961238+ritikkumar8z@users.noreply.github.com>
2025-07-16 20:45:14 +0530

    Daily update

--- index.js
+++ index.js
@@ -5,17 +5,14 @@ import random from "random";
 
 const path = "./data.json";
 
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
+// 🟡 Fixed start date: 5 July 2023
+const START_DATE = moment("2023-07-05");
+const END_DATE = moment("2025-07-05");
 
-  const data = {
-    date: date,
-  };
+const markCommit = (x, y) => {
+  const date = START_DATE.clone().add(x, "w").add(y, "d").format();
+
+  const data = { date };
 
   jsonfile.writeFile(path, data, () => {
     simpleGit().add([path]).commit(date, { "--date": date }).push();
@@ -23,17 +20,21 @@ const markCommit = (x, y) => {
 };
 
 const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+  if (n === 0) return simpleGit().push();
 
-  const data = {
-    date: date,
-  };
-  console.log(date);
+  const x = random.int(0, 103); 
+  const y = random.int(0, 6);   
+  const date = START_DATE.clone().add(x, "w").add(y, "d");
+
+  if (date.isAfter(END_DATE)) return makeCommits(n); // Retry if beyond July 5, 2025
+
+  const formattedDate = date.format();
+  console.log(formattedDate);
+
+  const data = { date: formattedDate };
+
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+    simpleGit().add([path]).commit(formattedDate, { "--date": formattedDate }, makeCommits.bind(this, --n));
   });
 };
 
